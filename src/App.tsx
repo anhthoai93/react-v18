abdae1bcb22462629a2c2abd66f305a48b541908
AndyTran <thoai.tran@advahealthsolutions.com>
2022-04-13 00:33:41 +0700

    update title

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import CountDown from "./components/CountDown";
 function App() {
   return (
       <div>
+          <h1>Welcome to the board</h1>
           <CountDown
               timeTillDate={new Date()}
               timeFormat="MM DD YYYY, h:mm a"
